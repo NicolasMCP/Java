4cdf4a9ca41e36de91306821f46e154f058655ef
NicolasMCP <nicolas.mcp@gmail.com>
2018-08-17 12:01:17 -0300

    Incluindo Fibonacci (elemento dado da sequencia, ou sequencia até o elemento definido)

--- Basic/src/xyz/infodata/Math.java
+++ Basic/src/xyz/infodata/Math.java
@@ -1,4 +1,7 @@
 package xyz.infodata;
+
+import java.util.ArrayList;
+
 /*
  * Autor: Nicolas Ramos
  * Nota : Iniciando uma classe para incluir itens matematicos.
@@ -6,20 +9,67 @@ package xyz.infodata;
 public class Math {
 
     // recursivo
-    public double fatorial(double n){
-        if(n < 0.0) { return -1.0;}
-        if(n == 0.0 || n == 1.0) { return 1.0;}
-        return n * fatorial(n-1.0);
+    public long fatorial(long n){
+        if(n < 0L) { return -1L;}
+        if(n == 0L || n == 1L) { return 1L;}
+        return n * fatorial(n-1L);
     }
 
     // interativo
-    public double factorial(double n) {
-        if(n < 0.0) { return -1.0;}
-        if(n == 0.0 || n == 1.0) { return 1.0;}
-        double factorial = 1;
-        for(double i=2; i<=n; i++) {
+    public long factorial(long n) {
+        if(n < 0L) { return -1L;}
+        if(n == 0L || n == 1L) { return 1L;}
+        long factorial = 1;
+        for(long i=2; i<=n; i++) {
             factorial *= i;
         }
         return factorial;
     }
+
+    // recursivo, devolve o elemento solicitado da sequencia de fibonacci
+    public long elemFibonacci(long elemento) {
+        if(elemento < 0L || elemento == 0L) { return -1L;}
+        if(elemento == 1L) { return 0L;}
+        if(elemento == 2L) { return 1L;}
+        if(elemento == 3L) { return 1L;}
+        return elemFibonacci(elemento - 1L) + elemFibonacci(elemento - 2L);
+    }
+
+    // interativo, devolve o elemento solicitado da sequencia de fibonacci
+    public long elementoFibonacci(long elemento) {
+        long a1 = 0L;
+        long a2 = 1L;
+        long a3 = 1L;
+        if(elemento < 0L || elemento == 0L) { return -1L;}
+        if(elemento == 1L) { return a1;}
+        if(elemento == 2L) { return a2;}
+        for(int i=3; i<=elemento; i++) {
+            a3 = a1 + a2;
+            a1 = a2;
+            a2 = a3;
+        }
+        return a3;
+    }
+
+    // interativo, devolve a sequencia de fibonacci até o elemento solicitado
+    public ArrayList seqFibonacci(long elemento) {
+        long a1 = 0L;
+        long a2 = 1L;
+        long a3;
+        ArrayList<Long> seq = new ArrayList();
+        if(elemento < 0L || elemento == 0L) { return seq;}
+        seq.add(a1);
+        if(elemento == 1L) { return seq;}
+        seq.add(a2);
+        if(elemento == 2L) { return seq;}
+        for(int i=3; i<=elemento; i++) {
+            a3 = a1 + a2;
+            seq.add(a3);
+            a1 = a2;
+            a2 = a3;
+        }
+        return seq;
+    }
+
+
 }
--- Basic/src/xyz/infodata/testes/MathTest.java
+++ Basic/src/xyz/infodata/testes/MathTest.java
@@ -1,93 +1,179 @@
 package xyz.infodata.testes;
 
+import org.junit.jupiter.api.Test;
+
 import xyz.infodata.Math;
 
-import org.junit.jupiter.api.Test;
+import java.util.Arrays;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class MathTest {
 
     @Test
-    void fatorialNegativo() {
+    void fatorialRecursivoNegativo() {
         Math math = new Math();
-        assertEquals(-1.0, math.fatorial(-10.0));
+        assertEquals(-1L, math.fatorial(-10L));
     }
     @Test
-    void fatorial0() {
+    void fatorialRecursivo0() {
         Math math = new Math();
-        assertEquals(1.0, math.fatorial(0.0));
+        assertEquals(1L, math.fatorial(0L));
     }
     @Test
-    void fatorial1() {
+    void fatorialRecursivo1() {
         Math math = new Math();
-        assertEquals(1.0, math.fatorial(1.0));
+        assertEquals(1L, math.fatorial(1L));
     }
     @Test
-    void fatorial2() {
+    void fatorialRecursivo2() {
         Math math = new Math();
-        assertEquals(2.0, math.fatorial(2.0));
+        assertEquals(2L, math.fatorial(2L));
     }
     @Test
-    void fatorial3() {
+    void fatorialRecursivo4() {
         Math math = new Math();
-        assertEquals(6.0, math.fatorial(3.0));
+        assertEquals(24L, math.fatorial(4L));
     }
     @Test
-    void fatorial4() {
+    void fatorialRecursivo5() {
         Math math = new Math();
-        assertEquals(24.0, math.fatorial(4.0));
-    }
-    @Test
-    void fatorial5() {
-        Math math = new Math();
-        assertEquals(120.0, math.fatorial(5.0));
-    }
-    @Test
-    void fatorialRecursivo100() {
-        Math math = new Math();
-        assertEquals(9.33262154439441E157, math.fatorial(100.0));
+        assertEquals(120L, math.fatorial(5L));
     }
 
     @Test
-    void factorialInterativo100() {
-        Math math = new Math();
-        assertEquals(9.33262154439441E157, math.factorial(100.0));
-    }
-    @Test
     void factorialNegativo() {
         Math math = new Math();
-        assertEquals(-1.0, math.factorial(-10.0));
+        assertEquals(-1L, math.factorial(-10L));
     }
     @Test
     void factorial0() {
         Math math = new Math();
-        assertEquals(1.0, math.factorial(0.0));
+        assertEquals(1L, math.factorial(0L));
     }
     @Test
     void factorial1() {
         Math math = new Math();
-        assertEquals(1.0, math.factorial(1.0));
+        assertEquals(1L, math.factorial(1L));
     }
     @Test
     void factorial2() {
         Math math = new Math();
-        assertEquals(2.0, math.factorial(2.0));
-    }
-    @Test
-    void factorial3() {
-        Math math = new Math();
-        assertEquals(6.0, math.factorial(3.0));
+        assertEquals(2L, math.factorial(2L));
     }
     @Test
     void factorial4() {
         Math math = new Math();
-        assertEquals(24.0, math.factorial(4.0));
+        assertEquals(24L, math.factorial(4L));
     }
     @Test
     void factorial5() {
         Math math = new Math();
-        assertEquals(120.0, math.factorial(5.0));
+        assertEquals(120L, math.factorial(5L));
+    }
+
+    @Test
+    void fibonacciRecursivoNegativo() {
+        Math math = new Math();
+        assertEquals(-1L, math.elemFibonacci(-10L));
+    }
+    @Test
+    void fibonacciRecursivo0() {
+        Math math = new Math();
+        assertEquals(-1L, math.elemFibonacci(0L));
+    }
+    @Test
+    void fibonacciRecursivo1() {
+        Math math = new Math();
+        assertEquals(0L, math.elemFibonacci(1L));
+    }
+    @Test
+    void fibonacciRecursivo2() {
+        Math math = new Math();
+        assertEquals(1L, math.elemFibonacci(2L));
+    }
+    @Test
+    void fibonacciRecursivo3() {
+        Math math = new Math();
+        assertEquals(1L, math.elemFibonacci(3L));
+    }
+    @Test
+    void fibonacciRecursivo6() {
+        Math math = new Math();
+        assertEquals(5L, math.elemFibonacci(6L));
+    }
+    @Test
+    void fibonacciRecursivo7() {
+        Math math = new Math();
+        assertEquals(8L, math.elemFibonacci(7L));
+    }
+    @Test
+    void fibonacciRecursivo15() {
+        Math math = new Math();
+        assertEquals(377L, math.elemFibonacci(15L));
+    }
+
+    @Test
+    void elementoFibonacciNegativo() {
+        Math math = new Math();
+        assertEquals(-1L, math.elementoFibonacci(-10L));
+    }
+    @Test
+    void elementoFibonacci0() {
+        Math math = new Math();
+        assertEquals(-1L, math.elementoFibonacci(0L));
+    }
+    @Test
+    void elementoFibonacci1() {
+        Math math = new Math();
+        assertEquals(0L, math.elementoFibonacci(1L));
+    }
+    @Test
+    void elementoFibonacci2() {
+        Math math = new Math();
+        assertEquals(1L, math.elementoFibonacci(2L));
+    }
+    @Test
+    void elementoFibonacci3() {
+        Math math = new Math();
+        assertEquals(1L, math.elementoFibonacci(3L));
+    }
+    @Test
+    void elementoFibonacci6() {
+        Math math = new Math();
+        assertEquals(5L, math.elementoFibonacci(6L));
+    }
+    @Test
+    void elementoFibonacci7() {
+        Math math = new Math();
+        assertEquals(8L, math.elementoFibonacci(7L));
+    }
+
+    @Test
+    void seqFibonacciNegativa() {
+        Math math = new Math();
+        assertEquals(Arrays.asList(), math.seqFibonacci(0L));
+    }
+    @Test
+    void seqFibonacci0() {
+        Math math = new Math();
+        assertEquals(Arrays.asList(), math.seqFibonacci(0L));
+    }
+    @Test
+    void seqFibonacci1() {
+        Math math = new Math();
+        assertEquals(Arrays.asList(0L), math.seqFibonacci(1L));
+    }
+    @Test
+    void seqFibonacci2() {
+        Math math = new Math();
+        assertEquals(Arrays.asList(0L, 1L), math.seqFibonacci(2L));
+    }
+    @Test
+    void seqFibonacci8() {
+        Math math = new Math();
+        assertEquals(Arrays.asList(0L, 1L, 1L, 2L, 3L, 5L, 8L, 13L, 21L, 34L, 55L, 89L,
+                144L, 233L, 377L), math.seqFibonacci(15L));
     }
 
 }
