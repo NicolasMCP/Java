bdc73e16350e00753b81a477b568e30e4335b06e
NicolasMCP <nicolas.mcp@gmail.com>
2018-08-23 15:56:19 -0300

    Com construtor a classe ainda compila e roda. No entanto agora apresenta um valor totalmente diferente de 0, mesmo sem termos fornecido parametros.

--- Basic/src/xyz/infodata/construtor/Aplicacao.java
+++ Basic/src/xyz/infodata/construtor/Aplicacao.java
@@ -5,6 +5,12 @@ public class Aplicacao {
     private int parametroA;
     private int parametroB;
 
+    public Aplicacao() {
+        /* Parametros padrão (caso não sejam fornecidos) */
+        this.parametroA = 12;
+        this.parametroB = 16;
+    }
+
     public long Calcula() {
         return parametroA * parametroB + (parametroA + parametroB);
     }
