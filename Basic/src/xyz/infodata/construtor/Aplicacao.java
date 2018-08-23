f7e1e4f107733f268af42d9047140c36f29c679d
NicolasMCP <nicolas.mcp@gmail.com>
2018-08-23 15:51:58 -0300

    Sem construtor a classe compila e roda. Apresenta valor 0, porque não demos parametros.

---
+++ Basic/src/xyz/infodata/construtor/Aplicacao.java
@@ -0,0 +1,11 @@
+package xyz.infodata.construtor;
+
+public class Aplicacao {
+
+    private int parametroA;
+    private int parametroB;
+
+    public long Calcula() {
+        return parametroA * parametroB + (parametroA + parametroB);
+    }
+}
---
+++ Basic/src/xyz/infodata/construtor/Main.java
@@ -0,0 +1,12 @@
+package xyz.infodata.construtor;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Aplicacao ap = new Aplicacao();
+        long toPrint = ap.Calcula();
+
+        System.out.println("O resultado é: "+toPrint);
+    }
+}
