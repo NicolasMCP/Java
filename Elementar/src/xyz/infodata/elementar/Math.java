5270463557eec7729c9f96155c32e772cdf2087d
NicolasMCP <nicolas.mcp@gmail.com>
2018-11-11 23:04:04 -0200

    Java Elementar, Classe Math JavaDoc

--- Elementar/src/xyz/infodata/elementar/Math.java
+++ Elementar/src/xyz/infodata/elementar/Math.java
@@ -2,34 +2,67 @@ package xyz.infodata.elementar;
 
 import java.math.BigDecimal;
 import java.util.ArrayList;
-/*
- * Autor: Nicolas Ramos
- * Nota : Iniciando uma classe para incluir itens matematicos.
+
+/**
+ * A Classe Math tem por objetivo incluir vários métodos de calculo de itens matematicos.
+ * @author Nicolas Ramos {@link nicolas@infodata.xyz}
  *
  */
 public class Math {
 
-    // usando BigDecimal
+    /**
+     * Soma dois valores sem falha no arredondamento. Fazendo uso dos 'BigDecimal'.
+     * @param a Primeiro valor a ser somado
+     * @param b Segundo valor a ser somando<br>
+     * Os parametros podem ser do tipo 'BigDecimal' ou 'String'.
+     * @return O resultado da soma correto
+     */
     public BigDecimal somaBig(BigDecimal a, BigDecimal b) {
         return a.add(b);
     }
+    
     public BigDecimal somaBig(String a, String b) {
         BigDecimal num_a = new BigDecimal(a);
         BigDecimal num_b = new BigDecimal(b);
-        return num_a.add(num_b);
+        return somaBig(a, b);
     }
+    
+    public BigDecimal somaBig(BigDecimal a, String b) {
+    	BigDecimal num_b = new BigDecimal(b);
+    	return somaBig(a, b);
+    }
+    
+    public BigDecimal somaBig(String a, BigDecimal b) {
+    	BigDecimal num_a = new BigDecimal(a);
+    	return somaBig(a, b);
+    }
+
+    /**
+     * Soma dois valores, permitindo falhas de arredondamento por usar variáveis 'double'
+     * @param a Primeiro valor a somar
+     * @param b Segundo valor a somar
+     * @return O resultado da soma (as vezes com erros de arredondamento)
+     */
     public double somaDouble(double a, double b) {
         return a+b;
     }
 
-    // recursivo
+    /**
+     * Calcula o Fatorial, usando Recursividade
+     * @param n Valor a partir do qual será feito o calculo
+     * @return Fatorial de n
+     */
     public long fatorial(long n){
         if(n < 0L) { return -1L;}
         if(n == 0L || n == 1L) { return 1L;}
         return n * fatorial(n-1L);
     }
 
-    // interativo
+    /**
+     * Calcula o Fatorial, Interativo
+     * @param n Valor a partir do qual será feito o calculo
+     * @return Fatorial de n
+     */
     public long factorial(long n) {
         if(n < 0L) { return -1L;}
         if(n == 0L || n == 1L) { return 1L;}
@@ -40,7 +73,11 @@ public class Math {
         return factorial;
     }
 
-    // recursivo, devolve o elemento solicitado da sequencia de fibonacci
+    /**
+     * Sequencia de Fibonacci, usando Recursividade
+     * @param elemento Nº Sequencial do elemento da Sequencia de Fibonacci
+     * @return o elemento solicitado da Sequencia de Fibonacci
+     */
     public long elemFibonacci(long elemento) {
         if(elemento < 0L || elemento == 0L) { return -1L;}
         if(elemento == 1L) { return 0L;}
@@ -49,7 +86,11 @@ public class Math {
         return elemFibonacci(elemento - 1L) + elemFibonacci(elemento - 2L);
     }
 
-    // interativo, devolve o elemento solicitado da sequencia de fibonacci
+    /**
+     * Sequencia de Fibonacci, Interativo
+     * @param elemento Nº Sequencial do elemento da Sequencia de Fibonacci
+     * @return o elemento solicitado da Sequencia de Fibonacci
+     */
     public long elementoFibonacci(long elemento) {
         long a1 = 0L;
         long a2 = 1L;
@@ -65,8 +106,13 @@ public class Math {
         return a3;
     }
 
-    // interativo, devolve um ArrayList com a sequencia de fibonacci até o elemento solicitado
-    public ArrayList seqFibonacci(long elemento) {
+    /**
+     * Sequencia de Fibonacci, Interativo
+     * @param elemento Nº Sequencial do elemento da Sequencia de Fibonacci
+     * @return ArrayList com a Sequencia de Fibonacci até o elemento solicitado
+     */
+    @SuppressWarnings("rawtypes")
+	public ArrayList seqFibonacci(long elemento) {
         long a1 = 0L;
         long a2 = 1L;
         long a3;
