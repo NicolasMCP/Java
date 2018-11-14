ffa712c5e2b0f3e67b7dfb3a219a63a2fc08702d
NicolasMCP <nicolas.mcp@gmail.com>
2018-11-14 15:35:31 -0200

    usando JUnit (teste unitário) da Classe Data

--- jse/Elementar/src/xyz/infodata/elementar/data/test_junit/DataTest.java
+++ jse/Elementar/src/xyz/infodata/elementar/data/test_junit/DataTest.java
@@ -5,19 +5,19 @@ import static org.junit.Assert.assertEquals;
 
 import org.junit.jupiter.api.Test;
 
-/*
- * Autor: Nicolas Ramos
- * Data : 10/09/2018
- * Nota : Testes jUnit da classe Data
- */
 
 
 import xyz.infodata.elementar.data.Data;
 
+/**
+ * Testes jUnit da classe Data
+ * @author Nicolas Ramos {@link nicolas@infodata.xyz}<br>
+ * Java 11 (também testado em Java 8 anteriormente)
+ */
 class DataTest {
 
-    Data d = new Data(15, 1, 2018, 21, 15,3);
-    String strData = d.strData(Data.FORMATO_24H);
+    Data data = new Data(15, 1, 2018, 21, 15,3);
+    String strData = data.strData(Data.FORMATO_24H);
 
     @Test
     void dataHora24H() {
@@ -26,15 +26,22 @@ class DataTest {
 
     @Test
     void dataHora12H() {
-        strData = d.strData(Data.FORMATO_12H);
+        strData = data.strData(Data.FORMATO_12H);
         assertEquals(strData, "15/01/2018 09:15:03 PM");
     }
 
     @Test
     void soData() {
-        d = new Data(15, 1, 2018);
-        strData = d.strData(Data.FORMATO_12H);
-        assertEquals(strData, "15/01/2018");
+        data = new Data(24, 2, 2018);
+        strData = data.strData(Data.FORMATO_12H);
+        assertEquals(strData, "24/02/2018");
+    }
+
+    @Test
+    void soData0() {
+    	data = new Data(31, 10, 2018);
+    	strData = data.strData(0);
+    	assertEquals(strData, "31/10/2018");
     }
 
 }
