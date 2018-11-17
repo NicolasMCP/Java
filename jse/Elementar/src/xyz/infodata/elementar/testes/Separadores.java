8756ec9fe2803b04e7be3a24f20b7d2c7de17bdf
NicolasMCP <nicolas.mcp@gmail.com>
2018-11-17 20:46:29 -0200

    Alguns separadores independentes do SO

--- jse/Elementar/src/xyz/infodata/elementar/testes/Separadores.java
+++ jse/Elementar/src/xyz/infodata/elementar/testes/Separadores.java
@@ -4,7 +4,7 @@ import java.io.File;
 /**
  * Apenas ums testes de comandos java.
  * @author Nicolas Ramos {@link nicolas@infodata.xyz}
- *
+ * Java 11 (já testado em Java 8)
  */
 public class Separadores {
 
