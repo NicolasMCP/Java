b1695b22211ff0749975feea799351adfe4d73ae
NicolasMCP <nicolas.mcp@gmail.com>
2018-11-20 13:58:31 -0200

    Atalhos úteis no Eclipse

---
+++ jse/Elementar/src/xyz/infodata/elementar/testes/AtalhosApp.java
@@ -0,0 +1,12 @@
+package xyz.infodata.elementar.testes;
+
+public class AtalhosApp {
+
+	public static void main(String[] args) {
+		
+		AtalhosEclipse ae = new AtalhosEclipse();
+		
+		ae.mostrarAtalhos();
+	}
+
+}
