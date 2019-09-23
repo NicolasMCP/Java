74c2f3bc5e83f1ed451cbe8882f4006d39cbe706
NicolasMCP <nicolas.mcp@gmail.com>
2019-09-23 20:26:07 -0300

    Atalhos úteis no Eclipse

---
+++ jse/Elementar/src/xyz/infodata/elementar/atalhos/eclipse/AtalhosApp.java
@@ -0,0 +1,12 @@
+package xyz.infodata.elementar.atalhos.eclipse;
+
+public class AtalhosApp {
+
+	public static void main(String[] args) {
+		
+		AtalhosEclipse atalhosEclipse = new AtalhosEclipse();
+		
+		atalhosEclipse.mostrarAtalhos();
+	}
+
+}
---
+++ jse/Elementar/src/xyz/infodata/elementar/atalhos/eclipse/AtalhosEclipse.java
@@ -0,0 +1,48 @@
+package xyz.infodata.elementar.atalhos.eclipse;
+
+public class AtalhosEclipse {
+
+	public void mostrarAtalhos() {
+
+		// TODO resumo de comandos a incluir no infodata.xyz
+		System.out.println("Ctrl + d = 'deleta linha atual'");
+		System.out.println("Ctrl + 1 = 'Fix - Conserta problemas'");
+		System.out.println("Ctrl + Alt + Seta_Para_Cima = 'Duplica a linha atual (ou bloco selecionado) '");
+		System.out.println("Ctrl + Alt + Seta_Para_Baixo = 'Duplica a linha atual (ou bloco selecionado) '");
+		System.out.println("Alt + Seta_Para_Baixo ou Seta_Para_Cima= 'Move a linha atual (ou bloco selecionado) Para Baixo ou Para Cima (conforme a seta pressionada)'");
+		System.out.println("Ctrl + 3 = 'Pesquisa'");
+		System.out.println("Ctrl + o = 'Organiza os Imports (Incluindo necessários e deletando não usados)'");
+		System.out.println("// TODO anotação = 'Window > Show View > Task'");
+		System.out.println("sysout > Ctrl + Space = 'System.out.println()'");
+		System.out.println("Ctrl + s = 'Salvar'");
+		System.out.println("Ctrl + Space = 'Opções complemento'");
+		System.out.println("Ctrl + Shift + T = 'Abre tipos, pode-se ver o código fonte do Java JDK'");
+		
+		System.out.println();
+
+		System.out.println(
+				"* Para que funcione o atalho Ctrl + Alt + Seta_Para_Cima, é necessário excluir o mesmo atalho no ");
+		System.out.println("Linux Mint 19.1, Cinnamon:");
+		System.out.println(
+				"Configurações > Gerenciador de Janelas > Aba Teclado > Geral > Clicar em: 'Percorrer inversamente pelas janelas abertas' > botão: Limpar > Fechar");
+		
+		System.out.println();
+		System.out.println(
+				"* Para que funcione o atalho Ctrl + Alt + Seta_Para_Cima, é necessário excluir o mesmo atalho no ");
+		System.out.println("MX-Linux 18, ");
+		System.out.println(
+				"Configurações > Gerenciador de Janelas > Aba Teclado > Clicar em: ");
+		System.out.println("'Espaço de trabalho acima' > botão: Limpar > Clicar em: ");
+		System.out.println("'Espaço de trabalho abaixo' > botão: Limpar > Fechar");
+		
+		System.out.println();
+		
+		System.out.println(
+				"* Para que funcione o atalho Ctrl + Alt + Seta_Para_Baixo, é necessário excluir o mesmo atalho no linux mint, ");
+		System.out.println("Cinnamon:");
+		System.out.println(
+				"Configurações > Gerenciador de Janelas > Aba Teclado > Geral > Clicar em: 'Mostrar a Tela de Seleção de Janela' > botão: Limpar > Fechar");                          
+		
+	}
+
+}
