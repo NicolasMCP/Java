445fdc95644dfd9e02e4d60c5367442f882fe8d5
NicolasMCP <nicolas.mcp@gmail.com>
2019-09-26 20:27:39 -0300

    Softblue Java12 mod14 aula03

--- threads/implementando/ImplementandoThreads/UsarThreadViaInterfaces/src/xyz/infodata/interfaces/threads/mil/Main.java
+++ threads/implementando/ImplementandoThreads/UsarThreadViaInterfaces/src/xyz/infodata/interfaces/threads/mil/Main.java
@@ -1,4 +1,14 @@
 package xyz.infodata.interfaces.threads.mil;
+/*
+ * Autor: Nicolas Ramos
+ * Nota : Implementação que visa imitar a implementação do MODULO 14 AULA 03 da Softblue
+ *        Curso de Java 12 Fundamentos, mas em vez de fazer a implementação extendendo
+ *        a Threads e visto que o Java não permite herança múltipla, caso fosse necessário
+ *        usar herança de outra classe, por esse motivo fiz este código por meio da 
+ *        implementação de interface.
+ *        
+ *        Desenvolvido com openjdk 12.0.2
+ */
 
 public class Main {
 
