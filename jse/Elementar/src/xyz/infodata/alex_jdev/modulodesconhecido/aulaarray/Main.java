e038e7653e723702987020b5891a249c6ee3b8f1
NicolasMCP <nicolas.mcp@gmail.com>
2019-10-19 23:29:42 -0300

    Debug Array JDK 13 tudo certo

---
+++ jse/Elementar/src/xyz/infodata/alex_jdev/modulodesconhecido/aulaarray/Main.java
@@ -0,0 +1,19 @@
+package xyz.infodata.alex_jdev.modulodesconhecido.aulaarray;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double notas[] = new double[5];
+		
+		notas[0] = 9.6;
+		notas[1] = 9.7;
+		notas[2] = 9.8;
+		notas[3] = 9.9;
+		notas[4] = 10.0;
+		
+		System.out.println(notas[0]);
+		
+	}
+
+}
