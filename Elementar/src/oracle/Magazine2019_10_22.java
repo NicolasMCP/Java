c41683224e9bd3b69b1810e650db9571aa9a5abe
NicolasMCP <nicolas.mcp@gmail.com>
2019-10-22 12:45:20 -0300

    Strings multilinhas,com aspas e car.especiais

---
+++ Elementar/src/oracle/Magazine2019_10_22.java
@@ -0,0 +1,26 @@
+package oracle;
+
+/*
+ * Autor: Magazine Oracle.com
+ * 
+ * Nota : Para correto uso no eclipse veja as configurações
+ *        que você precissa fazer no package a.config.eclipse
+ *        principalmente 5-java-compiler.png
+ */
+
+public class Magazine2019_10_22 {
+
+	public static void main(String[] args) {
+		
+		// Exemplo citado na revista da oracle deste mês 22/10/2019
+		
+		String html = """
+				<HTML>
+				  <BODY>
+				    <H1>"Java 13 is here!"</H1>
+				  </BODY>
+				</HTML>""";
+		System.out.println(html);
+	}
+
+}
