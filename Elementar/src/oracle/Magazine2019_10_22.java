af0dfb563e49600659310f466333d358e650ce5c
NicolasMCP <nicolas.mcp@gmail.com>
2019-10-22 13:20:43 -0300

    Strings multilinhas,com aspas e car.especiais

--- Elementar/src/oracle/Magazine2019_10_22.java
+++ Elementar/src/oracle/Magazine2019_10_22.java
@@ -13,6 +13,7 @@ public class Magazine2019_10_22 {
 	public static void main(String[] args) {
 		
 		// Exemplo citado na revista da oracle deste mês 22/10/2019
+		// Multilinha, com caracteres especiais, e aspas...
 		
 		String html = """
 				<HTML>
